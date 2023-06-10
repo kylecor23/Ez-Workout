94269ef150d60bbcadde5f2561425d45948c4eb0
KyleCorcoran <kylecorcoran9494@gmail.com>
2023-06-10 12:52:36 -0230

    implenented JS to dynamically populate dropdown

--- script.js
+++ script.js
@@ -1,51 +1,23 @@
-
-/*const t2 = document.createElement ('select')
-t2.innerHTML = "<option>22</option>"
-document.body.appendChild(t2)
-
-
-
-
-let select = document.querySelector('#inputPrime');
-let result = document.querySelector('#test');
-     select.addEventListener('change', function () {
-        result.textContent = t2;
-        });*/
-
-/*
 var workouts = {
-    'cardio':['HIIT'],
-    'Strength':['Chest','Back','Legs']
-},
+    strength: ["Chest", "Back", "Legs"],
+    cardio: ["HIIT"]
+  };
 
-Prime = document.getElementById('#inputPrime')
-Sub = document.getElementById('#inputSub');
+const workoutSelection = document.querySelector('#inputPrime')
+const subSelection = document.querySelector('#inputSub');
 
-setOptions(Prime, Object.keys(inputPrime));
+workoutSelection.addEventListener('change', (e) => {
+    const categorySelected = e.target.value;
+    subSelection.innerHTML = '';
 
-setOptions(Sub, Prime[Prime_select.value]);
 
-Prime.addEventListener('change', function() {
-    setOptions(Sub, Prime[Prime_select.value])
+const subCategorySelection = workouts[categorySelected]
+
+for (let i=0; i < subCategorySelection.length; i++) {
+    const style = subCategorySelection[i];
+    const option = document.createElement('option')
+    option.textContent = style;
+    option.value = style;
+    subSelection.appendChild(option);
+}
 });
-
-function setOptions(dropDown, option) {
-    dropDown.innerHTML = '';
-    option.forEach(function(value) {
-        dropDown.innerHTML += '<option name="' + value + '">' + value + '</option>';
-    });
-}
-; */
-
-
-
-var mainInput = document.getElementById("inputPrime");
-var workoutStyle = ["strength", "Cardio"];
-
-for(var z = 0; z < workoutStyle.length; z++) {
-    var style = workoutStyle[z];
-    var xx = document.createElement("option");
-    xx.textContent = style;
-    xx.value = style;
-    mainInput.appendChild(xx)
-}
