51f2dd78caf53738de37864a822201a85e1675c2
KyleCorcoran <kylecorcoran9494@gmail.com>
2023-06-18 19:18:14 -0230

    gave function to weight option

--- script.js
+++ script.js
@@ -13,25 +13,48 @@ const result = document.querySelector('#results');
 const inputWeight = document.querySelector('#inputWeight')
  
 
+// target change in sub cat by option selected in main 
 
 workoutSelection.addEventListener('change', (e) => {
     const categorySelected = e.target.value;
     subSelection.innerHTML = '';
 
 
-const subCategorySelection = workouts[categorySelected]
+    const subCategorySelection = workouts[categorySelected]
 
-for (let i=0; i < subCategorySelection.length; i++) {
-    const style = subCategorySelection[i];
-    const option = document.createElement('option')
-    option.textContent = style;
-    option.value = style;
-    subSelection.appendChild(option);
-}
-}); 
+    for (let i=0; i < subCategorySelection.length; i++) {
+        const style = subCategorySelection[i];
+        const option = document.createElement('option')
+        option.textContent = style;
+        option.value = style;
+        subSelection.appendChild(option);
+    }
+    }); 
+
+// hooking up weight input and calculating ratio
+
+inputWeight.addEventListener('change', (e) =>{
+    const weightSelected = e.target.value
+
+    const minPercent = 65
+    const maxPercent = 100
+    const randomPercent = Math.floor(Math.random()*(((maxPercent - minPercent) / 5) + 1)) * 5 + minPercent;
+    var weightResult = Math.floor(weightSelected * (randomPercent / 100));
+    //console.log(weightResult);
+
+    const minWeight = Number(weightSelected * .50)
+    for (
+        let weightFinal = weightResult;
+        weightFinal >= minWeight;
+        weightFinal -= weightSelected * 0.1) {
+        console.log(weightFinal);
+        };
+   
+    });
 
 
 
+// submit button to generate results 
 button.addEventListener('click', function (event) {
     event.preventDefault();
     result.innerHTML = '';
@@ -50,4 +73,6 @@ button.addEventListener('click', function (event) {
         result.appendChild(wr);
     }
 }); 
+
+
 
