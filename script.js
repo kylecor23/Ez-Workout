a982d4518af75b323e5576a35a68e1e6c641ab20
KyleCorcoran <kylecorcoran9494@gmail.com>
2023-07-16 22:03:54 -0230

    added cide for access exer & update over all code

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 const workouts = {
     strength: ["Chest", "Back", "Legs"],
     cardio: ["HIIT"]
@@ -13,12 +14,16 @@ const result = document.querySelector('#results');
 const inputWeight = document.querySelector('#inputWeight')
  
 
+function handleSubmit(e) {
+    e.preventDefault();
+    alert("Handle submit")
+}
+
 // target change in sub cat by option selected in main 
 
 workoutSelection.addEventListener('change', (e) => {
     const categorySelected = e.target.value;
     subSelection.innerHTML = '';
-
 
     const subCategorySelection = workouts[categorySelected]
 
@@ -29,102 +34,116 @@ workoutSelection.addEventListener('change', (e) => {
         option.value = style;
         subSelection.appendChild(option);
     }
-    }); 
+}); 
 
-    const exercises = {
-        strength: {
-            chest: 'Bench Press',
-            back: 'Deadlift',
-            legs: 'Squat'
-        }
-    };
-       
-    const exerciseAccessories = {
-        chestExercise: ["Incline press", "Machine press", "Chest fly", "Chest dip", "Decline Press", "Floor press"],
-        armExercise: ["Hammer Curl","Bicep Curl","Zottman Curls","Ez bar curl","Dual Rope Pushdown","Tricep Overhead Extension","Single Handle Push Down","Skull Crushers"],
-        shoulderExercise:["Overhead Press","Face Pulls","Delt Fly","Lateral Raise","Y Raise","Push Press","Front Raise"],
-        backExercise:["Single Arm Row","Lat Pulldown","Staight Arm Pulldown", "Shrugs", "Fammer Walks","Rack Pull","Pull Up","Barbell Row","Yoke walk"],
-        legExercise:["lunge","Step-Ups","Bulgarian Split Squat","Leg Extention","Sled Push","Leg Press","Hamstring Curl","Seated Calf Raise","Standing Calf Raise","Romanian Deadlift","Nordic Hamstring Curl"],
-        coreExtercise:["Dead Bug","Sit-up","Leg raise","Abb Wheel","Cable Crunch","Hanging leg Raise","Side Plank","Front Plank","Stir the Pot","Russian Twists"],
-        cardioExercise:["Slam Ball","Jumping Jacks","Burpee","Jump Squat","Digs","Suacide Sprints","Sled Sprint","High Knees","Bum Kicks"," Kettle Bell Swing","Plank Jacks","Flutter Kicks","Box Jumps"],
-
-    };
+const exercises = {
+    strength: {
+        chest: 'Bench Press',
+        back: 'Deadlift',
+        legs: 'Squat'
+    }
+};
+        
       
-    function workoutDisplay(workoutSelection, subSelection) {
-        const selectedExercise = exercises[workoutSelection.value.toLowerCase()][subSelection.value.toLowerCase()];
-      
-        const workoutDisplay = document.createElement('p');
-        workoutDisplay.textContent = selectedExercise;
-        result.appendChild(workoutDisplay);
-      };
+function workoutDisplay(workoutSelection, subSelection) {
+    const selectedExercise = exercises[workoutSelection.value.toLowerCase()][subSelection.value.toLowerCase()];
+    
+    const workoutDisplay = document.createElement('p');
+    workoutDisplay.textContent = selectedExercise;
+    result.appendChild(workoutDisplay);
+}
 
-      
-      const reps ={
-        numberReps: {
-            65: '12',
-            70: '10',
-            75: '8',
-            80: '6',
-            85: '5',
-            90: '3',
-            95: '2',
-            100: '1'
-                }
-    };
-// used to take top weight of the day and assine a rep based of % to be liffted
-    function exercisesReps(randomPercent){
-        const displayReps = reps.numberReps[randomPercent];
+function toNumberDivisibleBy5(decimalNumber) {
+    const numberRounded = Math.round(decimalNumber * 100);
+    const divisibleBy5 = (Math.round(numberRounded / 5) * 5) / 100;
 
-        const exercisesReps =document.createElement('p');
-        exercisesReps.textContent = "4 x " + displayReps;
-        result.appendChild(exercisesReps);
-    };
+    return divisibleBy5;
+}
 
-// take the ammount of jumps tell weight results is hit and creats the amount of warm-up sets that needed to be printed. 
-    function warmUpReps(jumps){
-        const displayWarmUp = jumps;
-        for (let i = 0; i < jumps; i++) {
-        const warmUpReps = document.createElement('p');
-        warmUpReps.textContent = "1 x 5";
-        result.appendChild(warmUpReps);
-        };
-    };
-// hooking up weight input
+const weightPercentToReps = {
+    0.65: ['3', '12'],
+    0.70: ['3', '10'],
+    0.75: ['4', '8'],
+    0.80: ['5', '6'],
+    0.85: ['5', '5'],
+    0.90: ['3', '3'],
+    0.95: ['2', '2'],
+    1: ['1', '1'] // Will likely be for the last set
+}
 
+const exerciseAccessories = {
+    chestExercise: ["Incline press", "Machine press", "Chest fly", "Chest dip", "Decline Press", "Floor press"],
+    armExercise: ["Hammer Curl","Bicep Curl","Zottman Curls","Ez bar curl","Dual Rope Pushdown","Tricep Overhead Extension","Single Handle Push Down","Skull Crushers"],
+    shoulderExercise:["Overhead Press","Face Pulls","Delt Fly","Lateral Raise","Y Raise","Push Press","Front Raise"],
+    backExercise:["Single Arm Row","Lat Pulldown","Staight Arm Pulldown", "Shrugs", "Fammer Walks","Rack Pull","Pull Up","Barbell Row","Yoke walk"],
+    legExercise:["lunge","Step-Ups","Bulgarian Split Squat","Leg Extention","Sled Push","Leg Press","Hamstring Curl","Seated Calf Raise","Standing Calf Raise","Romanian Deadlift","Nordic Hamstring Curl"],
+    coreExtercise:["Dead Bug","Sit-up","Leg raise","Abb Wheel","Cable Crunch","Hanging leg Raise","Side Plank","Front Plank","Stir the Pot","Russian Twists"],
+    cardioExercise:["Slam Ball","Jumping Jacks","Burpee","Jump Squat","Digs","Suacide Sprints","Sled Sprint","High Knees","Bum Kicks"," Kettle Bell Swing","Plank Jacks","Flutter Kicks","Box Jumps"],
 
+}
+const accessoriesCategory = {
+    'chest' : ['chestExercise' , 'armExercise'],
+    'back' : ['shoulderExercise' , 'backExercise'],
+    'legs' : ['legExercise' , 'coreExtercise'],
+};
+
+function accessories(exerciseAccessories) {
+   
+   const subCategorySelected = subSelection.value.toLowerCase();
+   let randomCategory;
+   let setTime = 0;
+   
+while (setTime <= 3 ) {
+   if (subCategorySelected in accessoriesCategory) {
+    const categoryOptions = accessoriesCategory[subCategorySelected];
+    randomCategory = categoryOptions[Math.floor(Math.random() * categoryOptions.length)];
+   } else {
+    randomCategory = Object.keys(exerciseAccessories)[Math.floor(Math.random() * Object.keys(exerciseAccessories).length)];
+   }
+   const randomExercise = exerciseAccessories[randomCategory][Math.floor(Math.random() * exerciseAccessories[randomCategory].length)];
+   const accessories = document.createElement('p');
+   accessories.textContent = randomExercise;
+   result.appendChild(accessories);
+
+   setTime++;
+   } };
+
+    
 
 
 // submit button to generate results 
-button.addEventListener('click', function (event) {
+formInput.addEventListener('submit', function (event) {
     event.preventDefault();
     result.innerHTML = '';
 
-    workoutDisplay(workoutSelection, subSelection);
-    
-
     const weightSelected = inputWeight.value;
-    const minPercent = 65;
-    const maxPercent = 100;
-// randomly generates a top percentage based on inputed max weight to give top weight for workout session 
-    const randomPercent = Math.floor(Math.random() * (((maxPercent - minPercent) / 5) + 1)) * 5 + minPercent;
-    const weightResult = Math.floor(weightSelected * (randomPercent / 100));
-   //generating random workout max % to be lifted and warm-up weight 
-    const minWeight = Number(weightSelected * 0.5);
-// caculates how many jumps it takes for weight results to be hit 
-    const jumps = Math.ceil((weightResult - minWeight) / (weightSelected * 0.1));
+
+    const minPercentOfWeight = 0.65;
+    const maxPercentOfWeight = 0.85;
     
-    for (
-        let weightFinal = minWeight; 
-        weightFinal <= weightResult; 
-        weightFinal += weightSelected * 0.1){
+    // Gives a decimal between 0.65 and 0.85
+    const randomPercentage = (Math.random() * (maxPercentOfWeight - minPercentOfWeight)) + minPercentOfWeight;
+    // Generates a decimal number (as a percentage) whose hundredth number is 0 or 5
+    const startPercentage = toNumberDivisibleBy5(randomPercentage);
+
+    // A counter for the number sets
+    let setCount = 0;
+
+    while (setCount <= 3) { // the max number of sets, at which the iteration will stop. There's be 4 iterations (or sets) since the counter starts at 0 and ends at 3.
+        const percentAdded = setCount * 0.05; // percent is incremented by 0.05.
+        const setPercent = toNumberDivisibleBy5(startPercentage + percentAdded);
+        const setWeight = Math.round((setPercent * weightSelected) / 2.5) * 2.5;
+
         const weightDisplay = document.createElement('p');
-        weightDisplay.textContent = weightFinal;
+    
+        const [sets, reps] = weightPercentToReps[setPercent];
+        
+        const isLastSet = setCount === 3
+
+        weightDisplay.textContent = isLastSet ? `1 x 4: ${setWeight}lbs` : `${sets} x ${reps}: ${setWeight}lbs`;
         result.appendChild(weightDisplay);
-        };
 
-        warmUpReps(jumps);
-        exercisesReps(randomPercent);
-     
-}); 
-
-// create a object array for the workouts to be displayed 
+        setCount++; // Very important to add to prevent an infinite loop and crash your browser!
+    }   
+    accessories(exerciseAccessories);
+});
